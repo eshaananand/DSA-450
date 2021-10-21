72ebda8c0a8dd910673ed672daa59dbfae59c3f7
Fuckerburg <eshaananand2911@gmail.com>
2021-10-21 14:28:02 +0530

    Array Code Added

---
+++ code/7-cyclic.c
@@ -0,0 +1,37 @@
+//Cyclic Array
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Enter the size of array\n");
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nThe no stored are :\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    int a = arr[n-1];
+    for(int i = n-1 ; i>=0 ; i--)
+    {
+        arr[i]=arr[i-1];
+    }
+    arr[0] = a;
+
+    printf("\n\nThe cyclic arrray is :\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+
+}
